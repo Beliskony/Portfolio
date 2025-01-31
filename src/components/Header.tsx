c86d2d633acd649c11466b3c29e35efd7ec01d17
Axel NGuessan <nguessanaxel21@gmail.com>
2025-01-31 17:42:57 +0000

    correction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ const Header: React.FC = () => {
    const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-transparent h-20 w-full py-6 px-20 items-center justify-between flex flex-row border-b">
+    <nav className="bg-transparent h-20 w-full py-6 px-20 items-center justify-between flex flex-row border-b max-sm:px-2">
         {/*partieUne*/}
         <div>
             <a href="#">
-                <h3 className="text-3xl font-bold hover:scale-105 transition-all">Digital-Gick</h3>
+                <h3 className="text-3xl font-bold text-left hover:scale-105 transition-all">Digital-Gick</h3>
             </a>
         </div>
 
@@ -33,7 +33,7 @@ const Header: React.FC = () => {
             </li>
         </ul>
 
-        <div className="relative xl:hidden lg:hidden md:flex items-center justify-center">
+        <div className="relative xl:hidden lg:hidden md:flex items-center justify-between">
              <img src="https://img.icons8.com/?size=50&id=8113&format=png&color=000000" className="block cursor-pointer"
              onClick={() => setIsOpen(!isOpen)}/>
         </div>
