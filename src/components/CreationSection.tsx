6069276adc29e79f985c68985b9c2a9b468a641f
Axel NGuessan <nguessanaxel21@gmail.com>
2025-03-27 15:31:33 +0000

    ajout de lien externe sur les projets

--- src/components/CreationSection.tsx
+++ src/components/CreationSection.tsx
@@ -16,7 +16,7 @@ function CreationSection() {
        
        <div className="grid gap-y-5 md:grid-cols-2 lg:grid-cols-3 max-sm:space-y-5 max-sm:grid-cols-1">
         {[ 
-          {image:"https://i.pinimg.com/736x/3f/7f/1e/3f7f1e9486dc3acc9b196ae5c5155ffe.jpg", textExplicatif:"Un Site d'Entreprise qui Inspire Confiance et Professionnalisme", dam:150, temps:1},
+          {image:"/images/siteDeux.png", textExplicatif:"Un Site d'Entreprise qui Inspire Confiance et Professionnalisme", dam:150, temps:1, lien:"https://escargot-shop.vercel.app"},
           {image:"/images/siteUn.png",textExplicatif:"Un Univers Web Qui Parle de Vous et Pour Vous", dam:200, temps:2},
           {image:"https://i.pinimg.com/736x/04/ae/d6/04aed634429791c6a8e298ec28e31856.jpg",textExplicatif:"Élégance et Modernité : Un Site Vitrine qui Captive", dam:250, temps:3},
           {image:"https://i.pinimg.com/736x/60/3a/8d/603a8d0670813f2d0d99856ebf760397.jpg",textExplicatif:"Innovation Digitale : Quand Créativité et Performance se Rencontrent", dam:300, temps:4},
@@ -24,7 +24,7 @@ function CreationSection() {
           {image:"https://i.pinimg.com/736x/d2/43/0c/d2430c7a2b9300e369ab9a2a47cb37d7.jpg",textExplicatif:"Une Présence Digitale Unique pour Se Démarquer", dam:450, temps:6}
   
         ].map((creation, index) => (
-          <motion.div key={index} className="flex w-full justify-center items-center md:px-1 lg:px-1 2xl:px-2 max-sm:px-5" 
+          <motion.div key={index} className="flex w-full justify-center items-center md:px-1 lg:px-1 2xl:px-2 max-sm:px-5 object-contain" 
            initial={{x: "-100%"}} 
            animate={isVisible ? {x: 0 }: {x: "-100%"}} 
           transition={{
@@ -33,7 +33,8 @@ function CreationSection() {
             damping: creation.dam,
             duration: creation.temps,
          }}>
-          <Creation image={creation.image} textExplicatif={creation.textExplicatif} />
+          <a href={creation.lien} target="_blank">
+          <Creation image={creation.image} textExplicatif={creation.textExplicatif} /></a>
         </motion.div>
         ))}
        </div>      
--- src/components/boite/Creation.tsx
+++ src/components/boite/Creation.tsx
@@ -2,14 +2,16 @@ interface ProjetCreation{
     image: string;
     titre?: string;
     textExplicatif?: string;
+    lien?: string
 }
 
-const Creation: React.FC <ProjetCreation>=({image,titre,textExplicatif}) => {
+const Creation: React.FC <ProjetCreation>=({image,titre,textExplicatif,lien}) => {
   return (
     <div className='w-96 h-[550px] bg-white flex flex-col p-4 gap-y-4 text-center shadow max-sm:w-full max-sm:h-[620px]'>
+      <a href={lien}>
         <img src={image} className="h-96 w-full object-cover object-top" />
         <h3 className="text-xl font-bold text-[#0B162C] text-wrap">{titre}</h3>
-        <p className="text-wrap font-medium text-base">{textExplicatif}</p>
+        <p className="text-wrap font-medium text-base">{textExplicatif}</p></a>
     </div>
   )
 }
