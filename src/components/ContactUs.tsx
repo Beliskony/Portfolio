26bd15db9027aa7f8ac3ebd527595300d9ad9394
Axel NGuessan <nguessanaxel21@gmail.com>
2025-10-09 16:42:54 +0000

    Update ContactUs.tsx

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -64,7 +64,7 @@ const ContactUs = () => {
       <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl animate-pulse" />
       <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl animate-pulse delay-1000" />
       
-      <div className="relative container mx-auto px-4 sm:px-6 lg:px-8 py-20 xl:py-32">
+      <div className="relative container mx-auto px-2 sm:px-6 lg:px-8 py-20 xl:py-32">
         <div className="grid lg:grid-cols-2 gap-12 lg:gap-20 items-center">
           
           {/* Left Column - Contact Info & Text */}
